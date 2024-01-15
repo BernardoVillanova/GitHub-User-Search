3de3d3146872a8393eccdc1356af29e064f5189b
BernardoVillanova <bernardo.villasantana@gmail.com>
2024-01-15 13:20:15 -0300

    All functionalities are done

--- script/main.js
+++ script/main.js
@@ -9,38 +9,57 @@ new Vue({
             </div>
         </template>
         <template v-else>
-        <nav>
-            <strong v-on:click="exibirRepo">Repos <i class="numRepo">{{ numRepo }}</i></strong>
-            <strong v-on:click="exibirStar">Starred <i class="numRepo">{{ numStar }}</i></strong> 
-            <button id="back" v-on:click="mostrarInput">Return</button>
-        </nav>
+            <nav>
+                <strong :class="{ 'selected': selectedRepoType === 'repos' }" v-on:click="exibirRepo">Repos <i class="numRepo">{{ numRepo }}</i></strong>
+                <strong :class="{ 'selected': selectedRepoType === 'starred' }" v-on:click="exibirStar">Starred <i class="numRepo">{{ numStar }}</i></strong> 
+                <button id="back" v-on:click="mostrarInput">Return</button>
+            </nav>
         </template>
-        <div v-if="!showInput">
-            <h2>{{ userName }}</h2>
-            <img v-bind:src="avatar" alt="User Avatar"/>
+        <div class="profile-container" v-if="!showInput">
+            <div class="profile">
+            <div class="profile-image">
+                <img v-bind:src="avatar" alt="User Avatar" />
+            </div>
+            <div class="profile-info">
+                <span class="userName">{{ userName }}</span>
+                <span class="bioUser">{{ bio }}</span>
+            </div>
+            </div>
         </div>
-        <div v-for="repo in repos" :key="repo.id">
+        <div class="search" v-if="!showInput">
+                <input v-model="filter" placeholder="Filter by Name">
+                <button v-on:click="enviar"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+        <div v-for="repo in filteredRepos" :key="repo.id" id="repo">
             <h3><a :href="repo.html_url" target="_blank">{{ repo.name }}</a></h3>
             <p>{{ repo.description }}</p>
             <p><i class="fa-solid fa-code"></i>: {{ repo.language }}</p>
             <a :href="repo.html_url" target="_blank"></a>
-            <hr/>
         </div>
     </div>
+
     `,
     data() {
         return {
-            userName: '',
-            avatar: '',
             showInput: true,
+            filter: '',
             user: '',
             userName: '',
             avatar: '',
             bio: '',
             numRepo: '',
             numStar: '',
-            repos: []
+            repos: [],
+            originalRepos: [],
+            selectedRepoType: 'repos'
         }
+    },
+    computed: {
+        filteredRepos() {
+            return this.repos.filter(repo =>
+                repo.name.toLowerCase().includes(this.filter.toLowerCase())
+            );
+        },
     },
     methods: {
         enviar() {
@@ -50,14 +69,15 @@ new Vue({
                     this.userName = response.data.name;
                     this.avatar = response.data.avatar_url;
                     this.bio = response.data.bio;
-                    this.numRepo = response.data.public_repos; 
+                    this.numRepo = response.data.public_repos;
+                    this.followers = response.data.followers;
+                    this.selectedRepoType = 'repos';
 
                     return axios.get(response.data.repos_url)
                 })
-
                 .then(response => {                    
                     this.repos = response.data;
-
+    
                     this.repos.forEach(repo => {
                         axios.get(repo.url) 
                             .then(repoResponse => {
@@ -66,7 +86,13 @@ new Vue({
                             })
                             .catch(error => console.error('Erro ao obter informações do repositório:', error));
                     });
+                    this.originalRepos = [...this.repos];
                     this.showInput = false;
+                    
+                    return axios.get(`https://api.github.com/users/${this.user}/starred`)
+                })
+                .then(response => {
+                    this.numStar = response.data.length;
                 })
                 .catch(error => {
                     window.alert('Não foi possível encontrar o usuário. Por favor, insira um nome de usuário válido.');
@@ -78,24 +104,31 @@ new Vue({
         },
         exibirRepo() {
             axios
-                .get(`https://api.github.com/users/${this.user}/repos`)
-                .then((response) => {
-                    this.repos = response.data;
-                    this.showInput = false;
-                })
-                .catch((error) => {
-                    console.error('Erro ao buscar repositórios:', error);
-                });
+            .get(`https://api.github.com/users/${this.user}/repos`)
+            .then((response) => {
+                this.enviar(response);
+            })
+            .catch((error) => {
+                console.error('Erro ao buscar repositórios:', error);
+            });
         },
         exibirStar() {
             axios
-                .get(`https://api.github.com/users/${this.user}/starred`)
-                .then((response) => {
-                    this.repos = response.data;
-                    this.showInput = false;
-                })
-                .catch((error) => {
-                    console.error('Erro ao buscar repositórios favoritos:', error);
-                });
-        }
-}})
+            .get(`https://api.github.com/users/${this.user}/starred`)
+            .then((response) => {
+                this.repos = response.data;
+                this.originalRepos = [...this.repos];
+                this.showInput = false;
+                this.selectedRepoType = 'starred';
+            })
+            .catch((error) => {
+                console.error('Erro ao buscar repositórios favoritos:', error);
+            })
+        },
+        filtrarRepos() {
+            this.repos = this.originalRepos.filter(repo =>
+              repo.name.toLowerCase().includes(this.filter.toLowerCase())
+            );
+        },
+    },
+});
